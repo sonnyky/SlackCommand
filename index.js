7b89e3d8e66c84a4302309827aeb33d4fb20f727
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2017-06-13 18:57:53 +0900

    Add validation
    slack token and team id

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var slackToken = process.env.SLACK_TOKEN;
+var teamId = process.env.SLACK_TEAM_ID;
+
 //use port is set in the environment variable, or 9001 if it isn’t set.
 app.set('port', (process.env.PORT || 9001));
 
@@ -19,11 +22,13 @@ app.get('/', function(req, res){
 //app.post is triggered when a POST request is sent to the URL ‘/post’
 app.post('/post', function(req, res){
   //take a message from Slack slash command
-
-  
-var body = 'ph-iodata1 のアクセスポイント使ってね～　パスワードは : 6949246599971 モグ！'
-
-res.send(body);
+  var body = '本サービスをご利用できませんｍ ｍ' 
+  var token = req.query.token;
+  var requesterTeamId = req.query.team_id;
+  if(token === slackToken && teamId === requesterTeamId){
+    body = 'ph-iodata1 のアクセスポイント使ってね～　パスワードは : 6949246599971 モグ！'
+  }
+  res.send(body);
    
 });
 
